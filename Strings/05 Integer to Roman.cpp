e9ef186852f164d3825111376a2efaeb0fad2dc7
Astra <abhaypratapsinghwd@gmail.com>
2024-03-23 21:55:16 +0530

    Integer to Roman

---
+++ Strings/05 Integer to Roman.cpp
@@ -0,0 +1,20 @@
+  #include "iostream"
+#include "vector" 
+#include <algorithm>    
+#include <unordered_map> 
+
+using namespace std;
+int main(){
+    int num = 1994;
+    vector<string> symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    vector<int> values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    string ans="";
+    for(int i=0;i<values.size();i++){
+        while(num>=values[i]){
+            ans+= symbols[i];
+            num-=values[i];
+        }
+    }
+    cout <<"ans : " << ans << endl;
+    return 0;
+}
