466b9adda8aaa40ab0ba73e0d5c8b7e93562c37c
Astra <abhaypratapsinghwd@gmail.com>
2024-03-23 21:57:12 +0530

    Integer to Roman

--- Strings/05 Integer to Roman.cpp
+++ Strings/05 Integer to Roman.cpp
@@ -5,16 +5,26 @@
 
 using namespace std;
 int main(){
+    // Given an integer, convert it to a Roman numeral string.
     int num = 1994;
-    vector<string> symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-    vector<int> values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    string ans="";
-    for(int i=0;i<values.size();i++){
-        while(num>=values[i]){
-            ans+= symbols[i];
-            num-=values[i];
+
+    // Define the Roman numeral symbols and their corresponding values
+    vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    // Initialize an empty string to store the Roman numeral representation
+    string ans = "";
+
+    // Iterate through the values and symbols
+    for (int i = 0; i < values.size(); i++) {
+        // Repeat the current symbol while the number is greater than or equal to the current value
+        while (num >= values[i]) {
+            ans += symbols[i];
+            num -= values[i];
         }
     }
-    cout <<"ans : " << ans << endl;
+
+    // Output the Roman numeral representation
+    cout << "ans: " << ans << endl;
     return 0;
 }
