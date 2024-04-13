81853fb672b8581aa13605f78aa3173ef224f909
Astra <abhaypratapsinghwd@gmail.com>
2024-04-13 17:14:43 +0530

    Convert an array to linked list

---
+++ Linked List/TakeUForward/Convert an array to Linked List.cpp
@@ -0,0 +1,36 @@
+#include "iostream"
+#include "vector" 
+#include <algorithm>    
+#include <set> 
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    // Inserting the first element of array as head of Linked list
+    Node* head = new Node(arr[0]);
+    Node* currentNode = head;
+    for(int i=1; i<sizeof(arr) / sizeof(int);i++){
+        Node* temp = new Node(arr[i]);
+        currentNode->next = temp;
+        currentNode = temp;
+    }
+
+    // Iterating through Linked List
+    cout << "Elements in Linked List : ";
+    Node* traverseNode = head;
+    while(traverseNode !=nullptr){
+        cout << traverseNode->data << " ";
+        traverseNode = traverseNode->next;
+    }
+    return 0;
+}
