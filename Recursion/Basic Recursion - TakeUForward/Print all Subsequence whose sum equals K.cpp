ee0cedc6bf64790fdd55cdd32d886ea4a4a96344
Astra <abhaypratapsinghwd@gmail.com>
2024-05-15 05:36:08 +0530

    Print all subsequence whose sum equals K

---
+++ Recursion/Basic Recursion - TakeUForward/Print all Subsequence whose sum equals K.cpp
@@ -0,0 +1,39 @@
+#include "iostream"
+#include "vector" 
+#include <algorithm>    
+#include <set> 
+#include <stack> 
+using namespace std;
+
+
+// Time Complexity : O(2^n)
+// Space Complexity : O(log(n))
+void subSequence(int index, vector<int> sub){
+    int arr[] = {1,2,1};
+    int sum = 2;
+    int len = sizeof(arr)/sizeof(arr[0]);
+    if(index >= len){
+        int currentSum = 0;
+        for(int i = 0; i < sub.size(); i++){
+            currentSum += sub[i];
+        }
+        if(currentSum == sum){
+            cout << "SubSequence : ";
+            for(int i = 0; i < sub.size(); i++){
+                cout << sub[i] << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    sub.push_back(arr[index]);
+    subSequence(index+1,sub);
+    sub.pop_back();
+    subSequence(index+1,sub);
+}
+
+int main(){
+    vector<int> sub = {};
+    subSequence(0,sub);
+    return 0;
+}
