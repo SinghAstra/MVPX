87afbd3efae867b7f8bf486c4b78401da324d0fa
Astra <abhaypratapsinghwd@gmail.com>
2024-05-21 03:29:37 +0530

    Subarray sum equals K

---
+++ Arrays/LeetCode - TakeUForward/Subarray Sum Equals K.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include "vector" 
+#include <algorithm>    
+#include <unordered_map> 
+
+using namespace std;
+
+// Approach : Sliding Window
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
+int subArraySum(vector<int>& nums, int k) {
+    int maxLength = -1;
+    for(int i = 0; i < nums.size(); i++) {
+        int currentSum = 0;
+        for(int j = i; j < nums.size(); j++) {
+            currentSum += nums[j];
+            if(currentSum == k) {
+                maxLength = max(maxLength, j - i + 1);
+            }
+        }
+    }
+    return maxLength;
+}
+
+int main(){
+    vector<int> nums = {1,1,1};
+    int k = 2;
+    int ans = subArraySum(nums,2);
+    cout << ans << endl;
+    return 0;
+}
