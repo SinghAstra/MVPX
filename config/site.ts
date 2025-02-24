c9a955479ffebf72e19b1a6a23f18d238db22ef9
Astra <abhaypratapsinghwd@gmail.com>
2025-02-25 02:54:19 +0530

    Basic setup

---
+++ config/site.ts
@@ -0,0 +1,21 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "Blog-TemplateX",
+  description:
+    "A modern, minimalistic blogging platform template built with Next.js, MDX, and ContentLayer.",
+  tagline: "Share your thoughts, inspire the world.",
+  url: "https://blog-templatex.vercel.app",
+  features: [
+    "Markdown and MDX support",
+    "SEO-optimized pages",
+    "Customizable themes",
+    "ContentLayer integration",
+    "Responsive and fast",
+  ],
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra/blog-templatex",
+    linkedIn: "https://www.linkedin.com/in/singhastra",
+  },
+};
---
+++ interfaces/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = {
+  name: string;
+  description: string;
+  tagline: string;
+  url: string;
+  features: string[];
+  links: {
+    twitter: string;
+    github: string;
+    linkedIn: string;
+  };
+};
